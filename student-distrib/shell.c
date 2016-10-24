719524ed25d9470d7a19244abd64cd8ec2a0a288
Srujun Thanmay Gupta <theununtrium@gmail.com>
2016-10-24 12:38:38 -0500

    Shell updates

--- student-distrib/shell.c
+++ student-distrib/shell.c
@@ -4,11 +4,13 @@
 #include "filesystem.h"
 #include "terminal.h"
 #include "keyboard.h"
+#include "rtc.h"
 #include "lib.h"
 
-#define SBUFSIZE 33
+#define SBUFSIZE         33
+#define MAX_PRINT_LINE   80
 
-static const int8_t cmd_ls[2][5]  = {"ls ", "ls\0"};
+static const int8_t cmd_ls[2][4]  = {"ls ", "ls\0"};
 static const int8_t cmd_cat[2][5] = {"cat ", "cat\0"};
 static const int8_t cmd_rtc[2][5] = {"rtc ", "rtc\0"};
 
@@ -26,10 +28,14 @@ shell_loop()
     memset(buf, '\0', MAX_INPUT_BUFFER_SIZE);
     bytes_read = 0;
 
-    terminal_write(0, "Welcome to MazeOS!\n", 25);
+    uint8_t greeting[] = "Welcome to MazeOS!\n";
+    uint8_t prompt[] = "MazeOS $ ";
+    uint8_t unsupported[] = "Command not supported!\n";
+
+    terminal_write(0, greeting, strlen((int8_t *)greeting));
     while(1)
     {
-        terminal_write(0, "MazeOS $ ", 10);
+        terminal_write(0, prompt, strlen((int8_t *)prompt));
         /* blocks until we have complete input from the keyboard */
         bytes_read = keyboard_read(0, buf, MAX_INPUT_BUFFER_SIZE);
 
@@ -37,6 +43,8 @@ shell_loop()
         if (buf[0] == CTRL_L)
         {
             clear_setpos(0, 0);
+            /* clear the command buffer */
+            memset(buf, '\0', MAX_INPUT_BUFFER_SIZE);
             continue;
         }
 
@@ -63,12 +71,12 @@ shell_loop()
             paramsize = bytes_read - strlen(cmd_cat[0]);
             command_cat((uint8_t *)paramstart, paramsize);
         }
-        else if (!strncmp(cmd_cat[1], buf, strlen(cmd_cat[1])))
-        {
-            paramstart = buf + strlen(cmd_cat[1]);
-            paramsize = bytes_read - strlen(cmd_cat[1]);
-            command_cat((uint8_t *)paramstart, paramsize);
-        }
+        // else if (!strncmp(cmd_cat[1], buf, strlen(cmd_cat[1])))
+        // {
+        //     paramstart = buf + strlen(cmd_cat[1]);
+        //     paramsize = bytes_read - strlen(cmd_cat[1]);
+        //     command_cat((uint8_t *)paramstart, paramsize);
+        // }
         /* rtc */
         else if (!strncmp(cmd_rtc[0], buf, strlen(cmd_rtc[0])))
         {
@@ -82,8 +90,9 @@ shell_loop()
             paramsize = bytes_read - strlen(cmd_rtc[1]);
             command_rtc((uint8_t *)paramstart, paramsize);
         }
+        else if (buf[0] == '\0' || buf[0] == ' ' || buf[0] == '\n');
         else
-            terminal_write(0, "Command not supported!\n", 25);
+            terminal_write(0, unsupported, strlen((int8_t *)unsupported));
 
         /* clear the command buffer */
         memset(buf, '\0', MAX_INPUT_BUFFER_SIZE);
@@ -94,33 +103,36 @@ shell_loop()
 
 
 /*
- * TODO
+ * command_list
+ *   DESCRIPTION: Test to print the list of files in the filesystem and their
+ *                sizes
+ *   INPUTS: params - the string of parameters, paramsize - the length of the
+ *           params string
+ *   OUTPUTS: int
+ *   RETURN VALUE: zero - success, > 0 - failed
+ *   SIDE EFFECTS: Prints list of files to the screen
  */
 int
 command_list(const uint8_t * params, uint32_t paramsize)
 {
-    int32_t fd, cnt;
-    uint8_t buf[SBUFSIZE];
+    uint32_t fsize, i;
+    uint8_t buf[MAX_PRINT_LINE];
+    dentry_t d;
 
-    if (-1 == (fd = fs_open((uint8_t*)".")))
+    i = 0;
+
+    while (-1 != read_dentry_by_index(i, &d))
     {
-        terminal_write(0, "Directory open failed\n", 25);
-        return 2;
-    }
+        i++;
+        memset(buf, ' ', MAX_PRINT_LINE);
+        memcpy(buf, d.filename, SBUFSIZE);
 
-    uint8_t fname[FILENAME_SIZE + 1] = ".";
-    fs_desc_t fd_file;
-    fd_file.index = -1;
-    memcpy(&(fd_file.filename), fname, 33);
-
-    while (0 != (cnt = fs_read((int32_t)(&fd_file), buf, SBUFSIZE-1))) {
-        if (-1 == cnt) {
-	        terminal_write(0, "Directory entry read failed\n", 30);
-	        return 3;
-	    }
-	    buf[cnt] = '\n';
-	    if (-1 == terminal_write(1, buf, cnt + 1))
-	        return 3;
+        fsize = get_file_size(&d);
+        uint8_t extra[] = "  Size: ";
+        memcpy(buf + SBUFSIZE, extra, strlen((int8_t *)extra));
+        itoa(fsize, (int8_t *)(buf + SBUFSIZE + strlen((int8_t *)extra)), 10);
+        buf[MAX_PRINT_LINE - 1] = '\n';
+        terminal_write(1, buf, MAX_PRINT_LINE);
     }
 
     return 0;
@@ -133,32 +145,39 @@ command_list(const uint8_t * params, uint32_t paramsize)
 int
 command_cat(const uint8_t * params, uint32_t paramsize)
 {
+    uint8_t noparam[] = "Provide a filename!\n";
+    uint8_t nofile[] = "File not found!\n";
+    uint8_t read_failed[] = "File read failed!\n";
+
     if(paramsize == 0)
     {
-        terminal_write(1, (uint8_t*)"Provide a filename!\n", 20);
+        terminal_write(1, noparam, strlen((int8_t *)noparam));
         return -1;
     }
 
     int32_t fd, cnt;
-    uint8_t buf[5000];
+    uint8_t buf[1453];
+    memset(buf, '\0', 1453);
 
     if (-1 == (fd = fs_open(params)))
     {
-        terminal_write(1, (uint8_t*)"File not found\n", 15);
+        terminal_write(1, nofile, strlen((int8_t *)nofile));
         return 2;
     }
 
     fs_desc_t fd_file;
     fd_file.index = -1;
-    memcpy(&(fd_file.filename), params, paramsize);
+    memset(fd_file.filename, '\0', FILENAME_SIZE + 1);
+    memcpy(fd_file.filename, params, paramsize);
 
-    cnt = fs_read((int32_t)(&fd_file), buf, 5000);
+    cnt = fs_read((int32_t)(&fd_file), buf, 1453);
     if (-1 == cnt)
     {
-        terminal_write(1, (uint8_t*)"File read failed\n", 17);
+        terminal_write(1, read_failed, strlen((int8_t *)read_failed));
         return 3;
     }
     terminal_write(1, buf, cnt);
+    putc('\n');
 
     return 0;
 }
@@ -170,5 +189,34 @@ command_cat(const uint8_t * params, uint32_t paramsize)
 int
 command_rtc(const uint8_t * params, uint32_t paramsize)
 {
+    uint32_t freq = 2;
+    rtc_write(0, &freq, sizeof(uint32_t));
+    clear_setpos(0, 0);
+
+    while (1)
+    {
+        uint8_t buf[MAX_BUFFER_SIZE];
+        terminal_read(0, buf, MAX_BUFFER_SIZE);
+
+        if (buf[0] == CTRL_C)
+        {
+            putc('\n');
+            get_kb_buffer(buf);
+            return 0;
+        }
+
+        if (buf[0] == CTRL_A)
+        {
+            clear_setpos(0, 0);
+            freq *= 2;
+            if (freq > 1024)
+                freq = 2;
+            rtc_write(0, &freq, sizeof(uint32_t));
+        }
+
+        putc('R');
+        rtc_read(0, "", 0);
+    }
+
     return 0;
 }
