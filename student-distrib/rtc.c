acab19d9df5a763ebca44206cef6caf679b73689
Arnav15 <Agarwl13@illinois.edu>
2016-10-24 15:59:21 -0500

    Completed rtc - open(), close(), read(), write() (#19)
    * Completed rtc - open(), close(), read(), write()
    
    * Added documentation
    
    * Added types.h
    
    * Added types.h

--- student-distrib/rtc.c
+++ student-distrib/rtc.c
@@ -7,7 +7,9 @@
 #include "rtc.h"
 #include "i8259.h"
 #include "lib.h"
+#include "types.h"
 
+volatile int rtc_interrupt_occurred = 0;
 
 /*
  * rtc_init
@@ -44,7 +46,6 @@ rtc_init(void)
     enable_irq(RTC_IRQ);
 }
 
-
 /*
  * rtc_interrupt_handler
  *   DESCRIPTION: Handles the RTC interrupt request. Currently calls
@@ -63,10 +64,111 @@ rtc_interrupt_handler(void)
 
     /* test_interrupts(); */
 
-    /* this is to take care of interrupts after IRQ 8 */
+    /* this is to ensure Register C is read after IRQ 8 */
     outb(STATUS_REG_C, RTC_PORT1);
     inb(RTC_PORT2);
 
     send_eoi(RTC_IRQ);
     enable_irq(RTC_IRQ);
+}
+
+/*
+ * rtc_read
+ *   DESCRIPTION: Waits until an interrupt has occured.
+ *   INPUTS: fd     - ignored
+ *           buf    - ignored
+ *           nbytes - ignored
+ *   OUTPUTS: none
+ *   RETURN VALUE: 0
+ *   SIDE EFFECTS: none
+ */
+int32_t
+rtc_read(int32_t fd, void* buf, int32_t nbytes)
+{
+    while(!rtc_interrupt_occurred)
+    {
+
+    }
+    rtc_interrupt_occurred = 0;
+    return 0;
+}
+
+/*
+ * rtc_write
+ *   DESCRIPTION: Updates the frequency of the RTC interrupts
+ *                according to the given input.
+ *   INPUTS: fd     - ignored
+ *           buf    - frequency to set
+ *           nbytes - number of bytes being passed
+ *   OUTPUTS: none
+ *   RETURN VALUE: 0  - if succesful
+ *                 -1 - otherwise
+ *   SIDE EFFECTS: Changes frequency of the RTC interrupts
+ */
+int32_t
+rtc_write(int32_t fd, const void* buf, int32_t nbytes)
+{
+    uint set_divider;
+    uint32_t freq;
+    uint count;
+    char prev_saved;
+
+    if(nbytes != 4)
+        return -1;
+
+    memcpy(&freq, buf, sizeof(uint32_t));
+    
+    count = 0;
+    
+    /* set register STATUS_REG_A */
+    outb(STATUS_REG_A, RTC_PORT1);
+    prev_saved = inb(RTC_PORT2);
+
+    if(freq <= 0 || freq >= 1024)
+        return -1;
+
+    while(freq != 1)
+    {
+        if(freq % 2 != 0)
+            return -1;
+        
+        freq = freq/2;
+        count++;
+    }
+    
+    set_divider = DIVIDER_SETTING_CEIL - count;
+
+    outb(STATUS_REG_A, RTC_PORT1);
+    /* write rate to STATUS_REG_A */
+    outb((prev_saved & MASK_LOWER) | set_divider, RTC_PORT2);
+
+    return 0;
+}
+
+/*
+ * rtc_open
+ *   DESCRIPTION: Currently, does nothing
+ *   INPUTS: filename - ignored
+ *   OUTPUTS: none
+ *   RETURN VALUE: 0
+ *   SIDE EFFECTS: none
+ */
+int32_t
+rtc_open(const uint8_t* filename)
+{
+    return 0;
+}
+
+/*
+ * rtc_close
+ *   DESCRIPTION: Currently, does nothing
+ *   INPUTS: fd - ignored
+ *   OUTPUTS: none
+ *   RETURN VALUE: 0
+ *   SIDE EFFECTS: none
+ */
+int32_t
+rtc_close(int32_t fd)
+{
+    return 0;
 }
