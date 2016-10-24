375171d85e7259fa8450ecdff5ee60b516c8f71b
Srujun Thanmay Gupta <theununtrium@gmail.com>
2016-10-24 00:19:22 -0500

    Shell updates

--- student-distrib/shell.c
+++ student-distrib/shell.c
@@ -3,12 +3,14 @@
 #include "shell.h"
 #include "filesystem.h"
 #include "terminal.h"
+#include "keyboard.h"
+#include "lib.h"
 
 #define SBUFSIZE 33
 
-static const unsigned char cmd_ls[]  = "ls";
-static const unsigned char cmd_cat[] = "cat";
-static const unsigned char cmd_rtc[] = "rtc";
+static const int8_t cmd_ls[2][5]  = {"ls ", "ls\0"};
+static const int8_t cmd_cat[2][5] = {"cat ", "cat\0"};
+static const int8_t cmd_rtc[2][5] = {"rtc ", "rtc\0"};
 
 /*
  * TODO
@@ -19,21 +21,72 @@ static const unsigned char cmd_rtc[] = "rtc";
 int
 shell_loop()
 {
+    int8_t buf[MAX_INPUT_BUFFER_SIZE];
+    uint32_t bytes_read;
+    memset(buf, '\0', MAX_INPUT_BUFFER_SIZE);
+    bytes_read = 0;
+
+    terminal_write(0, "Welcome to MazeOS!\n", 25);
     while(1)
     {
-        uint8_t buf[MAX_INPUT_BUFFER_SIZE];
-        uint32_t bytes_read;
+        terminal_write(0, "MazeOS $ ", 10);
         /* blocks until we have complete input from the keyboard */
         bytes_read = keyboard_read(0, buf, MAX_INPUT_BUFFER_SIZE);
 
-        if (!strncmp(cmd_ls, buf, sizeof(cmd_ls)))
-            command_list(buf + 1 + sizeof(cmd_ls), bytes_read);
-        else if (!strncmp(cmd_cat, buf, sizeof(cmd_cat)))
-            command_cat(buf + 1 + sizeof(cmd_cat), bytes_read);
-        else if (!strncmp(cmd_rtc, buf, sizeof(cmd_rtc)))
-            command_rtc(buf + 1 + sizeof(cmd_rtc), bytes_read);
+        /* check control codes */
+        if (buf[0] == CTRL_L)
+        {
+            clear_setpos(0, 0);
+            continue;
+        }
+
+        /* check commands */
+        void * paramstart;
+        uint32_t paramsize;
+        /* ls */
+        if (!strncmp(cmd_ls[0], buf, strlen(cmd_ls[0])))
+        {
+            paramstart = buf + strlen(cmd_ls[0]);
+            paramsize = bytes_read - strlen(cmd_ls[0]);
+            command_list((uint8_t *)paramstart, paramsize);
+        }
+        else if (!strncmp(cmd_ls[1], buf, strlen(cmd_ls[1])))
+        {
+            paramstart = buf + strlen(cmd_ls[1]);
+            paramsize = bytes_read - strlen(cmd_ls[1]);
+            command_list((uint8_t *)paramstart, paramsize);
+        }
+        /* cat */
+        else if (!strncmp(cmd_cat[0], buf, strlen(cmd_cat[0])))
+        {
+            paramstart = buf + strlen(cmd_cat[0]);
+            paramsize = bytes_read - strlen(cmd_cat[0]);
+            command_cat((uint8_t *)paramstart, paramsize);
+        }
+        else if (!strncmp(cmd_cat[1], buf, strlen(cmd_cat[1])))
+        {
+            paramstart = buf + strlen(cmd_cat[1]);
+            paramsize = bytes_read - strlen(cmd_cat[1]);
+            command_cat((uint8_t *)paramstart, paramsize);
+        }
+        /* rtc */
+        else if (!strncmp(cmd_rtc[0], buf, strlen(cmd_rtc[0])))
+        {
+            paramstart = buf + strlen(cmd_rtc[0]);
+            paramsize = bytes_read - strlen(cmd_rtc[0]);
+            command_rtc((uint8_t *)paramstart, paramsize);
+        }
+        else if (!strncmp(cmd_rtc[1], buf, strlen(cmd_rtc[1])))
+        {
+            paramstart = buf + strlen(cmd_rtc[1]);
+            paramsize = bytes_read - strlen(cmd_rtc[1]);
+            command_rtc((uint8_t *)paramstart, paramsize);
+        }
         else
-            terminal_write("Command not supported!\n");
+            terminal_write(0, "Command not supported!\n", 25);
+
+        /* clear the command buffer */
+        memset(buf, '\0', MAX_INPUT_BUFFER_SIZE);
     }
 
     return 0;
@@ -51,19 +104,26 @@ command_list(const uint8_t * params, uint32_t paramsize)
 
     if (-1 == (fd = fs_open((uint8_t*)".")))
     {
-        terminal_write("Directory open failed\n");
+        terminal_write(0, "Directory open failed\n", 25);
         return 2;
     }
 
-    while (0 != (cnt = fs_read(fd, buf, SBUFSIZE-1))) {
+    uint8_t fname[FILENAME_SIZE + 1] = ".";
+    fs_desc_t fd_file;
+    fd_file.index = -1;
+    memcpy(&(fd_file.filename), fname, 33);
+
+    while (0 != (cnt = fs_read((int32_t)(&fd_file), buf, SBUFSIZE-1))) {
         if (-1 == cnt) {
-	        terminal_write("Directory entry read failed\n");
+	        terminal_write(0, "Directory entry read failed\n", 30);
 	        return 3;
 	    }
 	    buf[cnt] = '\n';
 	    if (-1 == terminal_write(1, buf, cnt + 1))
 	        return 3;
     }
+
+    return 0;
 }
 
 
@@ -73,7 +133,34 @@ command_list(const uint8_t * params, uint32_t paramsize)
 int
 command_cat(const uint8_t * params, uint32_t paramsize)
 {
-    
+    if(paramsize == 0)
+    {
+        terminal_write(1, (uint8_t*)"Provide a filename!\n", 20);
+        return -1;
+    }
+
+    int32_t fd, cnt;
+    uint8_t buf[5000];
+
+    if (-1 == (fd = fs_open(params)))
+    {
+        terminal_write(1, (uint8_t*)"File not found\n", 15);
+        return 2;
+    }
+
+    fs_desc_t fd_file;
+    fd_file.index = -1;
+    memcpy(&(fd_file.filename), params, paramsize);
+
+    cnt = fs_read((int32_t)(&fd_file), buf, 5000);
+    if (-1 == cnt)
+    {
+        terminal_write(1, (uint8_t*)"File read failed\n", 17);
+        return 3;
+    }
+    terminal_write(1, buf, cnt);
+
+    return 0;
 }
 
 
@@ -83,5 +170,5 @@ command_cat(const uint8_t * params, uint32_t paramsize)
 int
 command_rtc(const uint8_t * params, uint32_t paramsize)
 {
-    
+    return 0;
 }
--- student-distrib/shell.h
+++ student-distrib/shell.h
@@ -11,4 +11,10 @@
 
 int shell_loop();
 
+int command_list(const uint8_t * params, uint32_t paramsize);
+
+int command_cat(const uint8_t * params, uint32_t paramsize);
+
+int command_rtc(const uint8_t * params, uint32_t paramsize);
+
 #endif /* SHELL_H */
